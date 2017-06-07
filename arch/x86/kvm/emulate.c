d25fce6dbb83c92ce428334371594f370b23e255
Paolo Bonzini <pbonzini@redhat.com>
2017-06-07 15:13:14 +0200

    CVE-2017-7518: KVM: x86: fix singlestepping over syscall
    DivestOS backport: CVE-2017-7518/3.18/0006.patch

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -2341,6 +2341,7 @@ static int em_syscall(struct x86_emulate_ctxt *ctxt)
 		ctxt->eflags &= ~(EFLG_VM | EFLG_IF);
 	}
 
+	ctxt->tf = (ctxt->eflags & X86_EFLAGS_TF) != 0;
 	return X86EMUL_CONTINUE;
 }
 
