3e022ed14232e37f21ff4f838165b0ccf3f43ac3
ZhaoLong Wang <wangzhaolong1@huawei.com>
2023-02-06 09:10:09 +0800

    CVE-2023-52741: cifs: Fix use-after-free in rdata->read_into_pages()
    DivestOS backport: CVE-2023-52741/^6.2/0001.patch

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -2962,7 +2962,7 @@ cifs_uncached_read_into_pages(struct TCP_Server_Info *server,
 		rdata->got_bytes += result;
 	}
 
-	return rdata->got_bytes > 0 && result != -ECONNABORTED ?
+	return result != -ECONNABORTED && rdata->got_bytes > 0 ?
 						rdata->got_bytes : result;
 }
 
@@ -3435,7 +3435,7 @@ cifs_readpages_read_into_pages(struct TCP_Server_Info *server,
 		rdata->got_bytes += result;
 	}
 
-	return rdata->got_bytes > 0 && result != -ECONNABORTED ?
+	return result != -ECONNABORTED && rdata->got_bytes > 0 ?
 						rdata->got_bytes : result;
 }
 
